d9b0c686be2a6834a8355fc992bbe234c6bc6a04
Agustin Hidalgo <ahidalgo130@gmail.com>
2022-03-12 21:45:44 +0100

    getters/setters

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -2,7 +2,47 @@ package cuentas;
 public class CCuenta {
 
 
-    private String nombre;
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
+
+	public double getSaldoActual() {
+		return saldoActual;
+	}
+
+	public void setSaldoActual(double saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+
+	private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInteres;
